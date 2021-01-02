909e2794b5c4f8589a26538339b61c6e037ebdba
Piotr kacprzycki <piotrek634@wp.pl>
2021-01-02 13:48:57 +0100

    add function js and new picture header

--- main.js
+++ main.js
@@ -3,3 +3,16 @@ let age = 32;
 const city = "Katowicach";
  
 console.log(`Czesć nazywam się ${name} mam ${age}lata i mieszkam w ${city}`);
+
+let heading = document.querySelector('.heading-js');
+
+
+function auto (){
+
+   setTimeout(() => {
+        heading.innerHTML=('Razem budujemy lepszą przyszłość dzięki której otaczamy was tym co najważniejsze czyli spokój w zaciszu domowym ');    
+    }, 5000);
+     
+
+}
+auto()
