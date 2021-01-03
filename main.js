b79d449ffbd1d5fb0aebf1eefe31dc09fb10d9dd
Piotr kacprzycki <piotrek634@wp.pl>
2021-01-03 14:02:54 +0100

    add normalize.css styles

--- main.js
+++ main.js
@@ -4,15 +4,40 @@ const city = "Katowicach";
  
 console.log(`Czesć nazywam się ${name} mam ${age}lata i mieszkam w ${city}`);
 
-let heading = document.querySelector('.heading-js');
+// let heading = document.querySelector('.heading-js');
 
 
-function auto (){
+// function auto (){
 
-   setTimeout(() => {
-        heading.innerHTML=('Razem budujemy lepszą przyszłość dzięki której otaczamy was tym co najważniejsze czyli spokój w zaciszu domowym ');    
-    }, 5000);
+//    setTimeout(() => {
+//         heading.innerHTML=(' dzięki której otaczamy was tym co najważniejsze czyli spokój w zaciszu domowym ');    
+//     }, 5000);
      
 
-}
-auto()
+// }
+// auto()
+// const score = 50;
+
+
+// function calculate(multiply) {
+
+// multiply = multiply * 5;
+// console.log(`mam${multiply}`)
+
+
+//  return  multiply;
+    
+// };
+
+// const result = calculate(score)
+
+// console.log(result)
+
+
+
+
+
+
+
+
+
